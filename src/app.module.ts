e4245197b15f709827cf73cb240d2ceefc74fec1
NastyaGavrilova <gavrilovan61@gmail.com>
2022-09-27 18:37:41 +0300

    create note-app-api project

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://nastya:nastya1234@cluster0.jp3bvqi.mongodb.net/nestjs-demo?retryWrites=true&w=majority',
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
