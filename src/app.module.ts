b658a937424a39934bcf9336f69c1df0978ac52d
NastyaGavrilova <gavrilovan61@gmail.com>
2022-10-06 20:32:42 +0300

    connected ORM Sequelize, PostgreSQL database connection configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,26 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from "@nestjs/config"
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotesModule } from './notes/notes.module';
+import { SequelizeModule } from "@nestjs/sequelize";
+import { Note } from "./notes/notes.model"
 
 @Module({
-  imports: [NotesModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://nastya:nastya1234@cluster0.jp3bvqi.mongodb.net/Notes?retryWrites=true&w=majority',
-    ),
+  imports: [NotesModule, ConfigModule.forRoot({
+    envFilePath: '.env'
+  }), SequelizeModule.forRoot({
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    models: [Note],
+    autoLoadModels: true
+  })
+
   ],
   controllers: [AppController],
   providers: [AppService],
